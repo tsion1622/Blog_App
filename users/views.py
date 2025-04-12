8f871a2ee5158c8b305f60af6ba0c9b3b9f8ac91
Tsion Tamirat <tayu.16.enye@gmail.com>
2025-04-12 23:49:28 +0300

    implement user registration and profile views with form validation

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import UserRegisterForm
 
@@ -10,10 +11,14 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            return redirect('login')
         else:
             # Print form errors to check what's wrong
             print(form.errors)  # Or log this if you're using logging
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
